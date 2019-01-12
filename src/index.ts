8f772781e69cc757cb671f5810c46c0cbea584e7
patarapolw <patarapolw@gmail.com>
2019-01-12 14:51:02 +0700

    Update Kanjipedia URL

--- src/index.ts
+++ src/index.ts
@@ -14,10 +14,15 @@ document.getElementById("searchQ")!.addEventListener("keyup", function (event) {
 
         (window as any).external_definition.parseJapanese(q.q, getFunction).then((r: any) => {
             document.getElementById("kanjipedia")!.innerHTML = r.kanjipedia;
+
             const kanjipediaUrlEl = document.getElementById("kanjipedia-url") as HTMLAnchorElement;
-            kanjipediaUrlEl.href = r.kanjipediaUrl;
-            kanjipediaUrlEl.style.display = 'block';
-            
+            if (r.kanjipediaUrl !== null) {
+                kanjipediaUrlEl.href = r.kanjipediaUrl;
+                kanjipediaUrlEl.style.display = "block";
+            } else {
+                kanjipediaUrlEl.style.display = "none";
+            }
+
             const elWeblioVocab = document.getElementById("weblio-vocab")!;
             elWeblioVocab.innerHTML = "";
             r.weblio.vocab.forEach((el: string) => {
