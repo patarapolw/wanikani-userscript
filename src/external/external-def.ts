900470d6570ff7fa97fe24282591870013f9b8e2
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2020-05-09 16:19:13 +0700

    update

--- src/external/external-def.ts
+++ src/external/external-def.ts
@@ -10,11 +10,8 @@ declare global {
   }
 }
 
-let word = $('div#character').text().trim().replace(/する|〜/, '')
-const url = document.URL
-
 $.jStorage.listenKeyChange('currentItem', function () {
-  word = $('div#character').text().trim().replace(/する|〜/, '')
+  updateInfo()
 })
 
 async function scrape (url: string) {
@@ -29,98 +26,51 @@ async function scrape (url: string) {
 }
 
 async function updateInfo () {
-  const { kanjipedia, kanjipediaUrl, weblio, weblioUrl } = await parseJapanese(word, scrape)
-  if (kanjipedia.trim()) {
-    if (url.indexOf('vocabulary') !== -1 || url.indexOf('kanji') !== -1 || url.indexOf('radical') !== -1) {
-      $('<section class="kanjipedia"></section>').insertAfter('#information')
-    }
-    $('#item-info-col2').prepend('<section class="kanjipedia"></section>')
-    if (url.indexOf('lesson') !== -1) {
-      $('#supplement-kan-meaning .col2').prepend('<section class="kanjipedia"></section>')
-      $('#supplement-voc-meaning .col2').prepend('<section class="kanjipedia"></section>')
-      console.log('Prepended')
-    } else {
-      if ($.jStorage.get('questionType') === 'reading') $('.kanjipedia').css('display', 'none')
-    }
+  const word = $('div#character, span.japanese-font-styling-correction:first').text().trim().replace(/(する|〜)/, '')
+  console.log(word)
 
-    $('.kanjipedia').html(
+  if (!word) {
+    return
+  }
+
+  const { kanjipedia, kanjipediaUrl, weblio, weblioUrl } = await parseJapanese(word, scrape)
+  console.log($('#item-info-meaning-mnemonic, #supplement-voc-meaning, #supplement-kan-meaning, #information'))
+
+  if (kanjipedia.trim()) {
+    const $kanjipedia = $('<section class="kanjipedia"></section>')
+    $('#item-info-meaning-mnemonic, #supplement-voc-meaning, #supplement-kan-meaning, #information').prepend($kanjipedia)
+
+    if ($.jStorage.get('questionType') === 'reading') $('.kanjipedia').css('display', 'none')
+
+    $kanjipedia.html(
       kanjipedia +
       '<br><a href="' + kanjipediaUrl + '" target="_blank">Click for full entries</a>')
-    $('.kanjipedia').prepend('<h2>Kanjipedia Explanation</h2>')
+    $kanjipedia.prepend('<h2>Kanjipedia Explanation</h2>')
   }
 
   if (weblio.length > 0) {
-    if (url.indexOf('vocabulary') !== -1 || url.indexOf('kanji') !== -1 || url.indexOf('radical') !== -1) {
-      $('<section class="weblio"></section>').insertAfter('#information')
-    }
+    const $weblio = $('<section class="weblio"></section>')
 
-    $('#item-info-col2').prepend('<section class="weblio"></section>')
+    $('#item-info-meaning-mnemonic, #supplement-voc-meaning, #supplement-kan-meaning, #information').append($weblio)
 
-    if (url.indexOf('lesson') !== -1) {
-      $('#supplement-kan-meaning .col2').prepend('<section class="weblio"></section>')
-      $('#supplement-voc-meaning .col2').prepend('<section class="weblio"></section>')
-      console.log('Prepended')
-    } else {
-      if ($.jStorage.get('questionType') === 'reading') $('.weblio').css('display', 'none')
-    }
+    if ($.jStorage.get('questionType') === 'reading') $('.weblio').css('display', 'none')
 
-    weblio.html('')
+    $weblio.html('')
     weblio.map((w: string) => {
-      $('.weblio').append(w)
+      $weblio.append(w)
     })
-    $('.weblio').append('<br><a href="' + weblioUrl + '" target="_blank">Click for full entries</a>')
+    $weblio.append('<br><a href="' + weblioUrl + '" target="_blank">Click for full entries</a>')
 
-    $('.weblio').prepend('<h2>Weblio Explanation</h2>')
+    $weblio.prepend('<h2>Weblio Explanation</h2>')
   }
 }
 
-if (url.indexOf('vocabulary') !== -1 || url.indexOf('kanji') !== -1 || url.indexOf('radical') !== -1) {
-  word = $('span.japanese-font-styling-correction:first').text().trim().replace(/する|〜/, '')
-  updateInfo()
-}
-
-var observer = new MutationObserver(function (mutations) {
-  for (var i = 0; i < mutations.length; ++i) {
-    for (var j = 0; j < mutations[i].addedNodes.length; ++j) {
-      const node = mutations[i].addedNodes[j] as HTMLElement
-      if (node.id === 'item-info-meaning-mnemonic') {
-        updateInfo()
-      }
-    }
-  }
-})
-observer.observe($('#item-info-col2').get(0), { childList: true })
-
-// setup observer to change kanji info box contents for subsequent items
-var observer2 = new MutationObserver(function () {
-  word = $('div#character').text().trim().replace(/する|〜/, '')
-  $('.weblio').remove()
-  $('.kanjipedia').remove()
-  updateInfo()
-})
-observer2.observe($('div#character').get(0), { attributes: true, childList: true, characterData: true })
-
-var observer3 = new MutationObserver(function (mutations) {
-  for (var i = 0; i < mutations.length; ++i) {
-    for (var j = 0; j < mutations[i].addedNodes.length; ++j) {
-      const node = mutations[i].addedNodes[j] as HTMLElement
-      if (node.style.display === 'block') {
-        $('.weblio').css('display', 'block')
-        $('.kanjipedia').css('display', 'block')
-      }
-    }
-  }
-})
-observer3.observe($('#item-info-meaning-mnemonic').get(0), { attributes: true })
-
-// function nthIndex (str: string, pat: string, n: number) {
-//   var L = str.length; var i = -1
-//   while (n-- && i++ < L) {
-//     i = str.indexOf(pat, i)
-//     if (i < 0) break
-//   }
-//   return i
-// }
+$(document.body).on(
+  'ready',
+  '#item-info-meaning-mnemonic, #supplement-voc-meaning, #supplement-kan-meaning, #information',
+  () => {
+    updateInfo()
+  })
 
 // @ts-ignore
 try { $('.app-store-menu-item').remove(); $('<li class="app-store-menu-item"><a href="https://community.wanikani.com/t/there-are-so-many-user-scripts-now-that-discovering-them-is-hard/20709">App Store</a></li>').insertBefore($('.navbar .dropdown-menu .nav-header:contains("Account")')); window.appStoreRegistry = window.appStoreRegistry || {}; window.appStoreRegistry[GM_info.script.uuid] = GM_info; localStorage.appStoreRegistry = JSON.stringify(appStoreRegistry) } catch (e) {}
