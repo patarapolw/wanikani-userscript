205d7e3afe98b7997ed71230f2d5e13d7b3f44d0
Pacharapol Withayasakpunt <polv@polv.cc>
2022-02-11 02:04:07 +0700

    remove bootstrap

--- web/main.ts
+++ web/main.ts
@@ -4,9 +4,55 @@ const elLoading = document.getElementById('lds-ring-container')!
 
 const elForm = document.querySelector('form')!
 const elQ = elForm.querySelector('input[name="q"]') as HTMLInputElement
+const elReading = elForm.querySelector(
+  'input[name="reading"]'
+) as HTMLInputElement
+const elSource = elForm.querySelector(
+  'select[name="source"]'
+) as HTMLSelectElement
+
+function hideReadingInput() {
+  let parent = elReading.parentElement
+  while (parent) {
+    if (parent.classList.contains('field')) {
+      break
+    }
+  }
+
+  if (parent) {
+    if (elSource.value === 'kanjipedia') {
+      parent.classList.add('hidden')
+    } else {
+      parent.classList.remove('hidden')
+    }
+  }
+
+  if (elSource.value === 'kanjipedia') {
+    elQ.value = elQ.value.substring(0, 1)
+  }
+}
+
+hideReadingInput()
+
+elSource.addEventListener('change', () => {
+  hideReadingInput()
+})
+
+function hideKanjiSource() {
+  if (elQ.value.length > 1) {
+    elSource.value = 'weblio'
+  }
+  hideReadingInput()
+}
+
+hideKanjiSource()
+
+elQ.addEventListener('input', () => {
+  hideKanjiSource()
+})
 
 function getSection(id: string) {
-  const parent = document.querySelector(`#${id}`)!
+  const parent = document.querySelector(`.Output #${id}`)!
 
   return {
     parent,
@@ -17,8 +63,12 @@ function getSection(id: string) {
   }
 }
 
-const elKanjipedia = getSection('kanjipedia')
-const elWeblio = getSection('weblio')
+const elOut: Record<string, ReturnType<typeof getSection>> = {
+  kanjipedia: getSection('kanjipedia'),
+  weblio: getSection('weblio'),
+  alc: getSection('alc'),
+  goo: getSection('goo')
+}
 
 async function scrape(url: string): Promise<string> {
   elLoading.classList.remove('hidden')
@@ -34,10 +84,7 @@ async function scrape(url: string): Promise<string> {
   })
 }
 
-const lookupMap = new Map<
-  string,
-  Record<'kanjipedia' | 'weblio', IGetResult | null>
->()
+const lookupMap = new Map<string, Record<string, IGetResult | null>>()
 const extDef = new ExternalDefinition(scrape)
 
 elForm.onsubmit = async (evt) => {
@@ -46,35 +93,31 @@ elForm.onsubmit = async (evt) => {
   let q = elQ.value
 
   if (q) {
-    let v = lookupMap.get(q)
-    if (!v) {
-      v = { kanjipedia: null, weblio: null }
+    const v = lookupMap.get(q) || {}
+    if (typeof v[elSource.value] === 'undefined') {
+      v[elSource.value] = null
 
       lookupMap.set(q, v)
-      await Promise.all([
-        q.length === 1
-          ? extDef.kanjipedia(q).then((k) => (v!.kanjipedia = k))
-          : null,
 
-        extDef.weblio(q).then((w) => (v!.weblio = w))
-      ])
+      switch (elSource.value) {
+        case 'kanjipedia':
+          await extDef.kanjipedia(q).then((k) => (v.kanjipedia = k))
+          break
+        case 'weblio':
+          await extDef.weblio(q).then((k) => (v.weblio = k))
+      }
+
       lookupMap.set(q, v)
     }
 
-    if (v.kanjipedia) {
-      elKanjipedia.section.innerHTML = v.kanjipedia.html
-      elKanjipedia.aBlank.href = v.kanjipedia.url
-      elKanjipedia.parent.classList.remove('hidden')
-    } else {
-      elKanjipedia.parent.classList.add('hidden')
-    }
+    Object.values(elOut).map((v) => {
+      v.parent.classList.add('hidden')
+    })
 
-    if (v.weblio) {
-      elWeblio.section.innerHTML = v.weblio.html
-      elWeblio.aBlank.href = v.weblio.url
-      elWeblio.parent.classList.remove('hidden')
-    } else {
-      elWeblio.parent.classList.add('hidden')
+    if (v[elSource.value]) {
+      elOut[elSource.value].section.innerHTML = v[elSource.value]!.html
+      elOut[elSource.value].aBlank.href = v[elSource.value]!.url
+      elOut[elSource.value].parent.classList.remove('hidden')
     }
   }
 }
