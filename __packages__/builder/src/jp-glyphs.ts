308f5f19ee42c89943b1033d1c514491405ffeed
Pacharapol Withayasakpunt <polv@polv.cc>
2022-04-18 22:14:27 +0700

    add more youglish support

--- __packages__/builder/src/jp-glyphs.ts
+++ __packages__/builder/src/jp-glyphs.ts
@@ -1,6 +1,6 @@
 import { injectLangObserver } from './shared/inject-lang'
 
-const DEFAULT_LANG = 'ja-JP'
+let DEFAULT_LANG = 'ja-JP'
 
 let isInject = true
 
@@ -9,6 +9,24 @@ if (
   /[\./]youglish\.com$/.test(location.origin)
 ) {
   isInject = /japanese/.test(location.pathname)
+
+  if (!isInject) {
+    DEFAULT_LANG = 'ko-KR'
+    isInject = /korean/.test(location.pathname)
+  }
+
+  if (!isInject) {
+    const m = /chinese(\/([a-z]{2}))?\?$/.exec(location.pathname)
+    if (m) {
+      DEFAULT_LANG = `zh-${(m[1] || 'CN').toLocaleUpperCase()}`
+      isInject = true
+    }
+  }
+
+  if (!isInject) {
+    DEFAULT_LANG = 'zh-CN'
+    isInject = /chinese/.test(location.pathname)
+  }
 }
 
 if (isInject) {
