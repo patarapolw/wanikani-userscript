5c7b03987ee47882689c725064e954ca89cd2d8c
Pacharapol Withayasakpunt <polv@polv.cc>
2022-05-02 10:14:13 +0700

    extension .user.js is required for auto-detection

---
+++ js/ime-injector.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         WanaKana IME Injector
+// @namespace    polv
+// @version      0.1
+// @description  WanaKana IME Injector
+// @author       polv
+// @match        https://gearoid.me/pokemon/
+// @require      https://unpkg.com/wanakana
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  const INJECTION_LIST = ['input#pokemonGuess'];
+
+  injectObserver();
+
+  function injectObserver(el = document.body) {
+    new MutationObserver((muts) => {
+      for (const mut of muts) {
+        mut.addedNodes.forEach((n) => {
+          if (n instanceof HTMLElement) {
+            inject(n);
+          }
+        });
+      }
+    }).observe(el, {
+      childList: true,
+      subtree: true,
+    });
+    inject(el);
+  }
+
+  function inject(el) {
+    INJECTION_LIST.map((inj) => {
+      if (el.matches(inj)) {
+        wanakana.bind(el);
+      }
+      el.querySelectorAll(inj).forEach((it) => {
+        wanakana.bind(it);
+      });
+    });
+  }
+})();
