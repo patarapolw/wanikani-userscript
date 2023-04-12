e28ebac8e10066102a728649df07bce4c14364d6
Pacharapol Withayasakpunt <polv@polv.cc>
2023-04-12 07:18:28 +0700

    (external def, states) updated item info

--- userscripts/external-definition.user.js
+++ userscripts/external-definition.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WaniKani JJ External Definition
 // @namespace    http://www.wanikani.com
-// @version      1.2.1
+// @version      1.3.0
 // @description  Get JJ External Definition from Weblio, Kanjipedia
 // @author       polv
 // @author       NicoleRauch
@@ -10,7 +10,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=weblio.jp
 // @license      MIT
 // @require      https://unpkg.com/dexie@3/dist/dexie.js
-// @require      https://greasyfork.org/scripts/430565-wanikani-item-info-injector/code/WaniKani%20Item%20Info%20Injector.user.js?version=1166918
+// @require      https://greasyfork.org/scripts/430565-wanikani-item-info-injector/code/WaniKani%20Item%20Info%20Injector.user.js?version=1173815
 // @grant        GM_xmlhttpRequest
 // @connect      kanjipedia.jp
 // @connect      weblio.jp
@@ -715,13 +715,7 @@
         if (state.type === 'vocabulary') {
           kanji = '';
           if (vocab !== fixedCharacters) {
-            reading =
-              state.reading ||
-              Array.from(
-                document.querySelectorAll('.reading-with-audio__reading'),
-              )
-                .map((el) => (el.textContent || '').trim())
-                .filter((s) => s);
+            reading = state.reading;
             vocab = fixedCharacters;
 
             updateInfo();
