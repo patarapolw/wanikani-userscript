d4491c8f6168cd3fbbccb5b6d4d1f3ff9cd12dc2
Pacharapol Withayasakpunt <polv@polv.cc>
2023-04-14 06:31:15 +0700

    (kun-on) first working

--- userscripts/kun-on.user.js
+++ userscripts/kun-on.user.js
@@ -1,167 +1,119 @@
 // ==UserScript==
 // @name        WK Custom Review Question (KunOn+)
-// @namespace   WK_CustomQuestion
+// @namespace   WK_KunOn
 // @description Changes the text of the Review or Lesson Quiz question. Specifies ON or KUN reading for Kanji. Options to have the text in English or Japanese.
-// @author      hoovard
-// @include     https://www.wanikani.com/review/session*
-// @include     http://www.wanikani.com/review/session*
-// @include     https://www.wanikani.com/lesson/session*
-// @include     http://www.wanikani.com/lesson/session*
-// @version     0.4.3
+// @author      polv (originally hoovard)
+// @match       https://www.wanikani.com/extra_study/session*
+// @match       https://www.wanikani.com/review/session*
+// @match       https://www.wanikani.com/subjects/review*
+// @match       https://www.wanikani.com/subjects/extra_study*
+// @match       https://www.wanikani.com/subjects/lesson/quiz*
+// @match       https://preview.wanikani.com/extra_study/session*
+// @match       https://preview.wanikani.com/review/session*
+// @match       https://preview.wanikani.com/subjects/review*
+// @match       https://preview.wanikani.com/subjects/extra_study*
+// @match       https://preview.wanikani.com/subjects/lesson/quiz*
+// @icon        https://www.google.com/s2/favicons?sz=64&domain=wanikani.com
+// @homepage    https://github.com/patarapolw/wanikani-userscript/blob/master/userscripts/kun-on.user.js
+// @version     1.0.0
 // @license     Do what you want with it (Preferably improve it).
 // @grant       none
 // ==/UserScript==
-// Version 0.4.3 applies to Reviews and Lesson Quizzes.
 
-// Language options
-// English "en", Japanese "ja"
-var strLang = 'en';
+// @ts-check
+(function () {
+  'use strict';
 
-// Tested on the following:
-// Firefox 35.0.1 and Chrome 39.0.2171.95 (64-bit), Linux Mint 17.1 Cinnamon 64-bit
-// Firefox 35.0.1 and Chrome 40.0.2214.115 m, Windows 8.1 64-bit
+  /**
+   * Language options
+   * English "en", Japanese "ja"
+   */
+  let strLang = 'en';
 
-// Thanks to Rui Pinheiro (LordGravewish) for the original script
-// and to Ethan for the idea to use MutationObserver to detect changes in the DOM.
+  // Vars to compose the replacement question string
+  let strKanji = 'Kanji';
+  let strRadical = 'Radical';
+  let strVocab = 'Vocabulary';
+  let strMeaning = 'Meaning';
+  let strReading = 'Reading';
+  let strOn = "On'yomi";
+  let strKun = "Kun'yomi";
+  let strNan = 'Nanori';
+  let strName = 'Name';
 
-// Vars to compose the replacement question string
-var strKanji;
-var strRadical;
-var strVocab;
-var strMeaning;
-var strReading;
-var strVocabReading;
-var strOn;
-var strKun;
-var strNan;
-var strName;
-
-// Translations
-switch (strLang) {
-  case 'en':
-    strKanji = 'kanji';
-    strRadical = 'radical';
-    strVocab = 'vocabulary';
-    strMeaning = 'meaning';
-    //strReading = "yomi";
-    strVocabReading = 'reading';
-    strOn = "on'yomi";
-    strKun = "kun'yomi";
-    strNan = 'nanori';
-    strName = 'name';
-    break;
-  case 'ja':
-    strKanji = '漢字';
-    strRadical = '部首';
-    strVocab = '単語';
-    strMeaning = '意味';
-    //strReading = "読み";
-    strVocabReading = '読み';
-    strOn = '音読み';
-    strKun = '訓読み';
-    strNan = '名乗り';
-    strName = '名前';
-    break;
-}
-
-// Variable to save and check against the previous contents of the jStorage item
-var objSavedCurrentItem;
-
-// Review or Lesson Quiz. jStorage objects are different.
-bIsReview = $(location).attr('href').indexOf('review') != -1;
-
-// Code based on Stack Overflow to detect changes in the DOM.
-// (http://stackoverflow.com/questions/3219758/detect-changes-in-the-dom/14570614#14570614)
-var observeDOM = (function () {
-  var MutationObserver = window.MutationObserver;
-
-  return function (obj, callback) {
-    if (MutationObserver) {
-      // define a new observer
-      var obs = new MutationObserver(function (mutations, observer) {
-        callback();
-      });
-      // have the observer observe for changes in children
-      // need to observe attributes because after we alter the question, wanikani will
-      // not update the question text in certain circumstances and instead only changes the class
-      // because WK uses regex to do a replacement on the text which results in no change if the
-      // text does not have the string WK is searching for (this behavior occurs in lesson quiz)
-      obs.observe(obj, { childList: true, attributes: true, subtree: true });
-    }
-  };
-})();
-
-// Callback function observing the 'question-type' div 'h1' element
-var selector = '#question-type h1';
-var observeMe = $(selector); // on review page
-if (observeMe.length == 0) {
-  // on lesson quiz page
-  selector = '#question-type';
-  observeMe = $(document); // on lessons page, selector isn't in dom on page load
-}
-observeMe = observeMe[0];
-let in_call = false;
-observeDOM(observeMe, function () {
-  let el = $(selector);
-  if (el.length == 0) return;
-
-  var objCurItem;
-  if (bIsReview) {
-    objCurItem = $.jStorage.get('currentItem');
-  } else {
-    objCurItem = $.jStorage.get('l/currentQuizItem');
-  }
-
-  var strQuestionType;
-  if (bIsReview) {
-    strQuestionType = $.jStorage.get('questionType');
-  } else {
-    strQuestionType = $.jStorage.get('l/questionType');
-  }
-
-  var strItemType = '';
-  var strReadingType = 'Reading';
-
-  // Compose the string elements to be sent into the h1 element
-  if ('kan' in objCurItem) {
-    // Kanji
-    strItemType = strKanji;
-    if (strQuestionType == 'reading') {
-      if (objCurItem.emph == 'onyomi') strReadingType = strOn;
-      else if (objCurItem.emph == 'kunyomi') strReadingType = strKun;
-      else if (objCurItem.emph == 'nanori') strReadingType = strNan;
-    } else {
-      strReadingType = strMeaning;
-    }
-  } else if ('voc' in objCurItem) {
-    // Vocabulary
-    strItemType = strVocab;
-    if (strQuestionType == 'reading') {
-      strReadingType = strVocabReading;
-    } else {
-      strReadingType = strMeaning;
-    }
-  } else if ('rad' in objCurItem) {
-    // Radical
-    strItemType = strRadical;
-    strReadingType = strName;
-  }
-
-  // replace the contents of #question-type h1
-  let newContents;
+  // Translations
   switch (strLang) {
-    case 'en':
-      newContents = strItemType + ' <strong>' + strReadingType + '</strong>';
-      break;
     case 'ja':
-      newContents = strReadingType;
+      strKanji = '漢字';
+      strRadical = '部首';
+      strVocab = '単語';
+      strMeaning = '意味';
+      strReading = '読み';
+      strOn = '音読み';
+      strKun = '訓読み';
+      strNan = '名乗り';
+      strName = '名前';
       break;
   }
 
-  // Prevent responding to own DOM changes
-  // triggers fewer mutationobservers compared to
-  // previous way of doing this
-  if (newContents != el.html()) {
-    $(selector).html(newContents);
-  }
-});
+  window.addEventListener('willShowNextQuestion', (e) => {
+    if ('detail' in e) {
+      const { subject, questionType } = /**
+       * @type {{
+       *  questionType: 'reading' | 'meaning',
+       *  subject: {
+       *    type: 'Radical' | 'Kanji' | 'Vocabulary',
+       *    primary_reading_type?: 'onyomi' | 'kunyomi' | 'nanori'
+       *  }
+       * }} */ (e.detail);
+
+      const el_category = document.querySelector(
+        '[data-quiz-input-target="category"]',
+      );
+      const el_questionType = document.querySelector(
+        '[data-quiz-input-target="questionType"]',
+      );
+
+      if (el_category && el_questionType) {
+        const obs_category = new MutationObserver(() => {
+          el_category.setAttribute('lang', strLang);
+          el_category.textContent = (() => {
+            switch (subject.type) {
+              case 'Radical':
+                return strRadical;
+              case 'Kanji':
+                return strKanji;
+            }
+            return strVocab;
+          })();
+          obs_category.disconnect();
+        });
+        obs_category.observe(el_category, { childList: true });
+
+        const obs_questionType = new MutationObserver(() => {
+          el_questionType.setAttribute('lang', strLang);
+          el_questionType.textContent = (() => {
+            if (questionType === 'reading') {
+              switch (subject.primary_reading_type) {
+                case 'onyomi':
+                  return strOn;
+                case 'kunyomi':
+                  return strKun;
+                case 'nanori':
+                  return strNan;
+              }
+              return strReading;
+            }
+
+            if (subject.type === 'Radical') return strName;
+
+            return strMeaning;
+          })();
+
+          obs_questionType.disconnect();
+        });
+        obs_questionType.observe(el_questionType, { childList: true });
+      }
+    }
+  });
+})();
