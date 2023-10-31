103528871d32c1af93fc4e5102b07c180ef24f80
Pacharapol Withayasakpunt <polv@polv.cc>
2023-10-31 14:11:18 +0700

    (even-kana) fix and publish

--- userscripts/even-kana.user.js
+++ userscripts/even-kana.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         WaniKani Do You Even Kana? (ModAnswerChecker register)
+// @name         WaniKani Even Kana? (ModAnswerChecker)
 // @namespace    http://www.wanikani.com
-// @version      1.1.0
+// @version      1.2.0
 // @description  Check that the okurigana matches the answer
 // @author       polv
 // @match        https://www.wanikani.com/extra_study/session*
@@ -13,9 +13,9 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=wanikani.com
 // @license      MIT
 // @require      https://greasyfork.org/scripts/470201-wanikani-answer-checker/code/WaniKani%20Answer%20Checker.js?version=1215595
-// @homepage     https://greasyfork.org/en/scripts/465750-wanikani-please-check-spelling
-// @supportURL   https://community.wanikani.com/t/userscript-plz-check-spelling-no-cigar-but-accept-plural-and-no-space-variants/61763
-// @source       https://github.com/patarapolw/wanikani-userscript/blob/master/userscripts/plz-check-spelling.user.js
+// @homepage     https://greasyfork.org/en/scripts/478704-wanikani-even-kana-modanswerchecker
+// @supportURL   https://community.wanikani.com/t/do-you-even-kana-okurigana-matcher/8440/56
+// @source       https://github.com/patarapolw/wanikani-userscript/blob/master/userscripts/even-kana.user.js
 // @grant        none
 // ==/UserScript==
 
@@ -24,13 +24,9 @@
 (function () {
   'use strict';
 
-  /**
-   * !Okurigana Matcher section
-   * @see https://community.wanikani.com/t/do-you-even-kana-okurigana-matcher/8440
-   */
   window.modAnswerChecker.register((e) => {
     if (e.questionType === 'reading' && e.item.type === 'Vocabulary') {
-      if (!makeRegex(e.item.characters).test(e.response.trim())) {
+      if (!matchOkurigana(e.item.characters, e.response.trim())) {
         return {
           action: 'retry',
           message: {
@@ -43,51 +39,30 @@
     return null;
   });
 
-  //Create regex profiles (katakana matches need hiragana counterparts included)
-  /** Prepends Hiragana counterpart to any Katakana string input
-   * @param {String} char - A one character long string that may be a Katakana character
-   * @returns {String} A single character if the input is Hiragana or "ー"; A two character string of (hopefully) Hiragana-Katakana pairs in square brackets (that can form a regex) if not.
-   * @bug Will attempt to pair any character that is not Hiragana or "ー"
+  const CP_KATA_A = 'ア'.charCodeAt(0);
+  const CP_HIRA_A = 'あ'.charCodeAt(0);
+
+  /**
+   * @param {string} s
+   * @returns {string}
    */
-  function pairKatakana(char) {
-    if (/^[\u3040-\u309fー]$/.test(char)) {
-      //is char hiragana or "ー"?
-      return char;
-    } else {
-      //set up pairs
-      var offset = -6 * 16; //katakana block: 30a0-30ff
-      var katakana = String.fromCharCode(char.charCodeAt(0) + offset);
-      return '[' + char + katakana + ']';
-    }
+  function toHiragana(s) {
+    return s.replace(/\p{sc=Katakana}/gu, (c) =>
+      ['ヶ'].includes(c)
+        ? c
+        : String.fromCharCode(c.charCodeAt(0) - CP_KATA_A + CP_HIRA_A),
+    );
   }
 
-  /** Returns true if the character is Kana
-   * @param {String} char
+  /**
+   *
+   * @param {string} key
+   * @param {string} userAnswer
+   * @returns {boolean}
    */
-  function isKana(char) {
-    return /^[\u3040-\u30ff]$/.test(char);
-  }
-
-  /** Creates regex from a vocabulary item that matches the Kana in that item.
-   * @param {string} cV
-   */
-  function makeRegex(cV) {
-    var r = '^'; //start the regex string
-    for (var c = 0; c < cV.length; c++) {
-      if (isKana(cV[c])) {
-        r += pairKatakana(cV[c]);
-      } else {
-        //we have a non-kana character
-        if (cV[c] !== '〜') {
-          //I doubt WK will be adding Kana suffixes but just covering all the bases to be safe.
-          r += '(.+)'; // unknown number of characters in reading (corresponding to kanji), capturing in groups for versatility
-          while (c < cV.length && !isKana(cV[c + 1])) {
-            c++; //skip non-kana characters (already have ".+" in our regex, do not need to add more)
-          }
-        }
-      }
-    }
-    r += '$'; // End of regex
-    return new RegExp(r);
+  function matchOkurigana(key, userAnswer) {
+    return new RegExp(
+      '^' + toHiragana(key).replace(/[^\p{sc=Hiragana}ー]+/gu, '.+') + '$',
+    ).test(toHiragana(userAnswer));
   }
 })();
