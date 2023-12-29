af7491200e98d52009882d9d511df42363151951
Pacharapol Withayasakpunt <polv@polv.cc>
2023-12-29 14:03:00 +0700

    add

---
+++ userscripts/unicode-url.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         WaniKani Community Unicode URI
+// @namespace    polv/wanikani-community
+// @version      0.1
+// @description  Fix URL encode
+// @author       polv
+// @match        *://community.wanikani.com/*
+// @require      https://unpkg.com/uri-js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=community.wanikani.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  document.addEventListener('click', async () => {
+    const text = await navigator.clipboard.readText();
+    if (text) {
+      await navigator.clipboard.writeText(
+        text.replace(/(%[0-9A-Z]{2})+/g, (r) =>
+          URI.normalize(r, { unicodeSupport: true }),
+        ),
+      );
+    }
+  });
+})();
